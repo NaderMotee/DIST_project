3eb253f0562c15ed63e9a13525f3db10077d8b06
JensD1 <jens.duym@student.uantwerpen.be>
2020-03-27 12:03:57 +0100

    Lab 2 Part 2 Try 1

--- src/main/java/org/example/TcpServer.java
+++ src/main/java/org/example/TcpServer.java
@@ -1,54 +1,70 @@
 package org.example;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.lang.Runnable;
 
-public class TcpServer {
+public class TcpServer implements Runnable{
     public static void main(String[] args) throws Exception {
-        //Initialize Sockets
-        System.out.println("Server started working.");
-        ServerSocket serverSocket = new ServerSocket(5000);
-        Socket clientSocket = serverSocket.accept();
+        Runnable runnable = new TcpServer();
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        Thread thread3 = new Thread(runnable);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
 
-        //The InetAddress specification
-        InetAddress serverIP = InetAddress.getByName("localhost"); // 127.0.0.1
+    @Override
+    public void run(){
+        while (true){
+            //Initialize Sockets
+            System.out.println("Server started working.");
+            ServerSocket serverSocket = null;
+            try {
+                serverSocket = new ServerSocket(5000);
+                Socket clientSocket = serverSocket.accept();
 
-        //Specify the file
-        File file = new File("Data/document.pdf");
-        FileInputStream fis = new FileInputStream(file); // Reads bytes from the file.
-        BufferedInputStream bis = new BufferedInputStream(fis); // Gives extra functionality to fileInputStream so it can buffer data.
+                //The InetAddress specification
+                InetAddress serverIP = InetAddress.getByName("localhost"); // 127.0.0.1
 
-        //Get clientSocket's output stream
-        OutputStream os = clientSocket.getOutputStream(); // Enables to send the data to the client
+                //Specify the file
+                File file = new File("Data/document.pdf");
+                FileInputStream fis = new FileInputStream(file); // Reads bytes from the file.
+                BufferedInputStream bis = new BufferedInputStream(fis); // Gives extra functionality to fileInputStream so it can buffer data.
 
-        //Read File Contents into contents array
-        byte[] contents;
-        long fileLength = file.length();
-        long current = 0;
+                //Get clientSocket's output stream
+                OutputStream os = clientSocket.getOutputStream(); // Enables to send the data to the client
 
-        long start = System.nanoTime();
-        while(current!=fileLength){
-            int size = 10000;
-            if(fileLength - current >= size)
-                current += size;
-            else{
-                size = (int)(fileLength - current);
-                current = fileLength;
+                //Read File Contents into contents array
+                byte[] contents;
+                long fileLength = file.length();
+                long current = 0;
+
+                long start = System.nanoTime();
+                while(current!=fileLength){
+                    int size = 10000;
+                    if(fileLength - current >= size)
+                        current += size;
+                    else{
+                        size = (int)(fileLength - current);
+                        current = fileLength;
+                    }
+                    contents = new byte[size];
+                    bis.read(contents, 0, size);
+                    os.write(contents);
+                    System.out.println("Sending file ... "+(current*100)/fileLength+"% complete!");
+                }
+
+                os.flush();
+                //File transfer done. Close the clientSocket connection!
+                clientSocket.close();
+                serverSocket.close();
+                System.out.println("File sent succesfully!");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            contents = new byte[size];
-            bis.read(contents, 0, size);
-            os.write(contents);
-            System.out.println("Sending file ... "+(current*100)/fileLength+"% complete!");
         }
-
-        os.flush();
-        //File transfer done. Close the clientSocket connection!
-        clientSocket.close();
-        serverSocket.close();
-        System.out.println("File sent succesfully!");
     }
 }
