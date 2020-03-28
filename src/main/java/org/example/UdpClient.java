5a25adfd96373d184108c4047f80f063951199bf
JensD1 <jens.duym@student.uantwerpen.be>
2020-03-28 12:32:26 +0100

    Lab 2 Task 3 Try 1

--- src/main/java/org/example/UdpClient.java
+++ src/main/java/org/example/UdpClient.java
@@ -2,31 +2,46 @@ package org.example;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class UdpClient {
     public static void main(String[] args) throws Exception{
-        //Initialize socket
+        //Initialize socket and send a wake up packet to server.
         System.out.println("Client started working.");
-        System.out.println(InetAddress.getByName("host2"));
-        Socket socket = new Socket(InetAddress.getByName("host2"), 5000);
-        byte[] contents = new byte[10000];
+        byte[] sentContent;
+        int size = 0;
+        sentContent = new byte[size];
+        DatagramSocket clientSocket = new DatagramSocket();
+        DatagramPacket sentPacket = new DatagramPacket(sentContent, size, InetAddress.getByName("host2"), 1234); // the ip address and port number to where we want to sent the data.
+        clientSocket.send(sentPacket);
 
+        // Now we will get the file from the server.
         //Initialize the FileOutputStream to the output file's full path.
         FileOutputStream fos = new FileOutputStream("Data/ReceivedData.pdf");
         BufferedOutputStream bos = new BufferedOutputStream(fos);
-        InputStream is = socket.getInputStream(); // I want to receive a file from the server.
 
+        byte[] receivedContent;
+        size = 10000;
+        receivedContent = new byte[size];
         //Number of bytes read in one read() call
         int bytesRead = 0;
-
-        while((bytesRead=is.read(contents))!=-1) // -1 ==> no data left to read.
-            bos.write(contents, 0, bytesRead); // content, offset, how many bytes are read.
-
+        DatagramPacket receivedPacket = new DatagramPacket(receivedContent, size);
+        clientSocket.receive(receivedPacket); // Receive first packet so the application can check if its not empty.
+        String received = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+        while(received.length() >= size){ // As long as you receive the max size stay in the while loop. The last time you'll receive something < 10000
+            bos.write(receivedContent);
+            clientSocket.receive(receivedPacket);
+        }
+        if(received.length() > 0){ // if the last time you've received something 10000>x>0, this needs to be written away.
+            bos.write(receivedContent);
+        }
         bos.flush();
-        socket.close();
-
+        clientSocket.close();
+        bos.close();
+        fos.close();
         System.out.println("File saved successfully!");
     }
 }
--- src/main/java/org/example/UdpServer.java
+++ src/main/java/org/example/UdpServer.java
@@ -1,20 +1,55 @@
 package org.example;
-import java.net.ServerSocket;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.DatagramPacket;
+import java.util.concurrent.Semaphore;
 
 public class UdpServer {
     public static void main(String[] args) throws Exception {
         try{
-            ServerSocket serverSocket = new ServerSocket(5000);
-            int counter=0;
-            System.out.println("Server Started ....");
-            while(true){
-                counter++;
-                Socket clientSocket=serverSocket.accept();  //server accept the client connection request
-                System.out.println(" >> " + "Client No:" + counter + " started!");
-                UdpServerClientThread sct = new UdpServerClientThread(clientSocket,counter); //send  the request to a separate thread
-                sct.start();
+            System.out.println("Server started working.");
+            byte[] receivedContent;
+            int size = 10000;
+            receivedContent = new byte[size];
+            DatagramPacket receivedPacket = new DatagramPacket( receivedContent, size);
+            DatagramSocket serverSocket = new DatagramSocket(1234);
+            serverSocket.receive(receivedPacket); // If there's a request, we save this request datagram packet in receivedPacket
+
+            InetAddress clientAddress = receivedPacket.getAddress(); // Returns the IP address of the machine to which this datagram is being sent or from which the datagram was received.
+            int clientPort = receivedPacket.getPort();
+
+            //Specify the file
+            File file = new File("Data/document.pdf");
+            FileInputStream fis = new FileInputStream(file); // Reads bytes from the file.
+            BufferedInputStream bis = new BufferedInputStream(fis); // Gives extra functionality to fileInputStream so it can buffer data.
+
+            // Specify new Datagram packet to send to the client
+            DatagramPacket sendPacket;
+            byte[] sendContents;
+            long fileLength = file.length();
+            long current = 0;
+            while(current!=fileLength) {
+                if(fileLength - current >= size)
+                    current += size;
+                else{
+                    size = (int)(fileLength - current);
+                    current = fileLength;
+                }
+                sendContents = new byte[size];
+                bis.read(sendContents, 0, size);
+                sendPacket = new DatagramPacket(sendContents, size, clientAddress, clientPort);
+                System.out.println("Sending file ... "+(current*100)/fileLength+"% complete!");
+                serverSocket.send(sendPacket);
             }
+            bis.close();
+            fis.close();
+            serverSocket.close();
+            System.out.println("File sent succesfully!");
         }catch(Exception e){
             System.out.println(e);
         }
